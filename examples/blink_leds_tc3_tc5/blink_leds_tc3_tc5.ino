0c69e37916ebd29c8e8817e0bda7cb2d4db53aa2
Daniel Mohr <daniel.mohr@uni-greifswald.de>
2022-09-15 21:58:21 +0200

    enhanced coding style in examples/blink_leds_tc3_tc5/blink_leds_tc3_tc5.ino

--- examples/blink_leds_tc3_tc5/blink_leds_tc3_tc5.ino
+++ examples/blink_leds_tc3_tc5/blink_leds_tc3_tc5.ino
@@ -13,24 +13,24 @@
 
 #define LED_PIN0 0
 #define LED_PIN1 1
-#define LED_PIN3 2
+#define LED_PIN2 2
 
 void TC3_Handler(void) {
   digitalWrite(LED_PIN0, !digitalRead(LED_PIN0));
-  digitalWrite(LED_PIN3, !digitalRead(LED_PIN3));
+  digitalWrite(LED_PIN2, !digitalRead(LED_PIN2));
   TC3->COUNT16.INTFLAG.bit.MC0 = 1; // clears the interrupt
 }
 
 void TC5_Handler(void) {
   digitalWrite(LED_PIN1, !digitalRead(LED_PIN1));
-  digitalWrite(LED_PIN3, !digitalRead(LED_PIN3));
+  digitalWrite(LED_PIN2, !digitalRead(LED_PIN2));
   TC5->COUNT16.INTFLAG.bit.MC0 = 1; // clears the interrupt
 }
 
 void setup() {
   pinMode(LED_PIN0, OUTPUT);
   pinMode(LED_PIN1, OUTPUT);
-  pinMode(LED_PIN3, OUTPUT);
+  pinMode(LED_PIN2, OUTPUT);
   fast_samd21_tc3_configure(101000); // starts the timer/trigger with 0.101 s
   fast_samd21_tc5_configure(100000); // starts the timer/trigger with 0.1 s
 }
