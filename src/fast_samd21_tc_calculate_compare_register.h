b3a2ac2fbb4afa8c35ab3c79e6d13e3383474c9e
Daniel Mohr <daniel.mohr@uni-greifswald.de>
2023-09-28 19:14:13 +0200

    fixed calculation of compare_register (enhanced precision)

--- src/fast_samd21_tc_calculate_compare_register.h
+++ src/fast_samd21_tc_calculate_compare_register.h
@@ -12,7 +12,7 @@
 static inline uint32_t
 _fast_samd21_tc_calculate_compare_register(double us,
 					   uint32_t prescaler) {
-  return (uint32_t) round(us * (SystemCoreClock / prescaler) / 1e6);
+  return (uint32_t) round(us * (SystemCoreClock / prescaler) / 1e6) - 1;
 }
 
 uint8_t fast_samd21_tc_calculate_compare_register(double us,
